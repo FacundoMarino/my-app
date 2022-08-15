9720c73f45353375f4c9fac7abe793b16b7d9c2d
FacundoMarino <facundoemarino@gmail.com>
2022-08-15 14:01:09 -0300

    cambios minimos

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -13,15 +13,18 @@ useEffect (() => {
  const db = getFirestore()
  const queryCollection = collection(db, 'items');
 
- if (categoryId) {
-    const queryCategoryId = query(queryCollection, where("category", "==", categoryId))
-    getDocs(queryCategoryId)
-    .then(resp => setData(resp.docs.map(item => ({id: item.id, ...item.data() }))))
-}
-else{
-    getDocs(queryCollection)
-    .then(resp => setData(resp.docs.map(item => ({id: item.id, ...item.data() })))) 
-}
+    if (categoryId) {
+
+        const queryCategoryId = query(queryCollection, where("category", "==", categoryId))
+        getDocs(queryCategoryId)
+        .then(resp => setData(resp.docs.map(item => ({id: item.id, ...item.data() }))))
+        
+    }
+    else{
+        
+        getDocs(queryCollection)
+        .then(resp => setData(resp.docs.map(item => ({id: item.id, ...item.data() })))) 
+    }
 
     }, [categoryId])
         
